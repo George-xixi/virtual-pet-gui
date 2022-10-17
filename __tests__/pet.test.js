7d83007ea3a248b0bc00ccd50c21bb31505376c4
George Aldington <georgealdington@gmail.com>
2022-10-17 14:09:58 +0100

    added hunger and fitness attributes to the pet, and they will be affected by the growUp function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,12 +1,59 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
+
   it('returns an object', () => {
     expect(new Pet('Fido')).toBeInstanceOf(Object);
   });
+
   it('returns an object with a property of name', () => {
     const pet = new Pet('Fido');
 
     expect(pet.name).toEqual('Fido');
   });
+
+  it('pet has an initial age of 0', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.age).toEqual(0);
+  });
+
+  it('pet has an intial hunger of 0', () => {
+    const pet = new Pet('Fido');
+    
+    expect(pet.hunger).toEqual(0);
+  });
+
+  it('pet has an intial fitness of 10', () => {
+    const pet = new Pet('Fido');
+    
+    expect(pet.fitness).toEqual(10);
+  });
 });
+
+describe('growUp', () => {
+
+  it('increases pet age by 1', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+    
+    expect(pet.age).toEqual(1);
+  })
+
+  it('increases pet hunger by 5', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+    
+    expect(pet.hunger).toEqual(5);
+  })
+
+  it('decreases pet fitness by 3', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+    
+    expect(pet.fitness).toEqual(7);
+  })
+})
